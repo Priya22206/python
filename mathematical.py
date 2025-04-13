ddcff35a2a3da7fe5ab71b16edf0f99633d28ffd
priya <priya22206@gmail.com>
2025-04-13 13:26:31 +0530

    added file for variable, datatypes,mathematical

---
+++ mathematical.py
@@ -0,0 +1,12 @@
+#mathematical opration
+x=20
+y=10
+add=x+y
+sub=x-y
+divide=x/y
+multiply=x*y
+print(add)
+print(sub)
+print(multiply)
+print(divide)
+print(30*20)
---
+++ variable.py
@@ -0,0 +1,13 @@
+#Variable
+#Constant: 2, 4, 6
+
+x = 5				# Integer
+y = 5.6				# Float
+name = "Priya"		# String
+
+print(x)
+print(y)
+print(name)
+
+name = "Ravi"
+print(name)
